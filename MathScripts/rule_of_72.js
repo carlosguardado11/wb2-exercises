04edcf7793ec371683a6a8e3992a6e935ef2df13
CarlosAGuardado <carlosguardado98@yahoo.com>
2023-04-25 13:58:16 -0400

    rule 72

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,5 @@
+var accountValue = 1500;
+var interestGain = 2.5
+var timeToDouble = 72/interestGain
+var valueDoubled = accountValue * 2
+console.log("at", interestGain, "interest rate your savings account will be worth ", valueDoubled, 'in', timeToDouble, 'years' )
